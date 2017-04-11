baa6013376c29d9240efc7d5db5cef60e8590624
Po Chen <chenpaul914@gmail.com>
2017-04-11 16:57:32 +1000

    v1.0.1
    Signed-off-by: Po Chen <chenpaul914@gmail.com>

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jwt.Mixfile do
 
   def project do
     [app: :yajwt,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
